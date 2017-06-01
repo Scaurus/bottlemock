56664e786faabdc7b11339768bfdf71f11c2ad1e
Роман Журавский <roman.zhuravskiy@lamoda.ru>
2017-06-01 18:23:59 +0300

    fix

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo docker run --name mock -p 7777:7777 -v /home/rzhuravskiy/www/mock/:/app/log/:ro -d bottlemock
+sudo docker run --name mock -p 7777:7777 -v /home/rzhuravskiy/www/mock/:/app/log/:rw -d bottlemock
